121b3f6894d4c66ae86b4a55a37827eea2745a96
jdub <jessicaclaing@gmail.com>
2024-03-07 15:23:32 -0800

    added bootstrap to client package.json

--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -5,11 +5,11 @@ const Footer = () => {
   const location = useLocation();
   const navigate = useNavigate();
   return (
-    <footer className="w-100 mt-auto bg-secondary p-4">
-      <div className="container text-center mb-5">
+    <footer className="container w-100 mt-auto">
+      <div className="text-center mb-5">
         {location.pathname !== '/' && (
           <button
-            className="btn btn-dark mb-3"
+            className="btn btn-dark my-3"
             onClick={() => navigate(-1)}
           >
             &larr; Go Back
