b01620f00eab59e0af37226eb287104b31743582
TreyLathe <147196984+TreyLathe@users.noreply.github.com>
2024-03-07 10:04:37 -1000

    added seed, question data

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,23 +1,26 @@
+const mongoose = require('mongoose');
+
 const db = require('../config/connection');
-const { Questions, Users } = require('../models'); // Import models here
+const { Question, User } = require('../models'); 
 const cleanDB = require('./cleanDB');
 
-const questionsData = require('./questions.json');
-const usersData = require('./users.json');
-//add other data here
+const questionData = require('./questions.json');
+const userData = require('./users.json');
+
 
 db.once('open', async () => {
     try {
         await cleanDB('Question', 'questions');
         await cleanDB('User', 'users');
-        // await cleanDB('', ''); //add other data here
-    
-        await Questions.create(questionsData);
-        await Users.create(usersData);
-        // await .create(); //add other data here
-    
+       
+        for(i=0; i <questionData.length; i++){
+            questionData[i]._id= new mongoose.Types.ObjectId();
+        }
+        await Question.collection.insertMany(questionData);
+        console.log(questionData);
         console.log('Seed data has been added!');
         process.exit(0);
+
     } catch (err) {
         console.error(err); 
     }
