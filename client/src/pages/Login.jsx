ce4b0aa52d3631c77fe89b22ab0832d7cc7afc2a
jdub <jessicaclaing@gmail.com>
2024-03-11 15:21:41 -0700

    updated title format to include all page titles

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -48,7 +48,7 @@ const Login = () => {
     <main className="login-container">
       <div className="segment">
         <div className="card">
-          <h2 className="login-title">Login</h2>
+          <h2 className="page-title">Login</h2>
           <div className="card-body">
             {data ? (
               <p>
